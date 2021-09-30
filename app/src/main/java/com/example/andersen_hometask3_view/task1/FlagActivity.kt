5afd5e28f6c7f98afb94890d8cf12baca2ad2a41
Coolmanakov <kulmanakovdenis@yandex.ru>
2021-09-30 17:12:21 +0300

    Add 8 flags, flag colors of this countries

---
+++ app/src/main/java/com/example/andersen_hometask3_view/task1/FlagActivity.kt
@@ -0,0 +1,12 @@
+package com.example.andersen_hometask3_view.task1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.andersen_hometask3_view.R
+
+class FlagActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flag)
+    }
+}
